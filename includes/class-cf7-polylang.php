533ceb945f5f9df72c71f44008746e8f88da119e
Aurovrata V <aurovrata@users.noreply.github.com>
2016-11-10 18:53:50 +0530

    v1.2.6 bump up

--- includes/class-cf7-polylang.php
+++ includes/class-cf7-polylang.php
@@ -69,7 +69,7 @@ class Cf7_Polylang {
 	public function __construct() {
 
 		$this->plugin_name = 'cf7-polylang';
-		$this->version = '1.2.5';
+		$this->version = '1.2.6';
 
 		$this->load_dependencies();
 		$this->set_locale();
